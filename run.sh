8e4c5c247ad60c8913302e530f310bd5ed3c0a95
NinV <quannl71290@gmail.com>
2020-09-11 01:31:36 +0900

    minor fix

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ echo "---------------------------------------------------"
 ROOT_PATH=$(pwd)
 
 # default batch_size = 14 for 2070 Super
-python3 detect_and_track_final_submission.py --input /data/test_data --weights detection/yolov5/weights/yolov5x.pt
+python3 detect_and_track_final_submission.py --input /data/test_data  --output counting/track_results --weights detection/yolov5/weights/yolov5x.pt
 
 # for 1080TI batch_size can be set to higher value
-# python3 detect_and_track_final_submission.py --input /data/test_data --weights detection/yolov5/weights/yolov5x.pt --batch_size 20
+# python3 detect_and_track_final_submission.py --input /data/test_data --output counting/track_results --weights detection/yolov5/weights/yolov5x.pt --batch_size 20
 
 cd counting
 python3 counting.py
