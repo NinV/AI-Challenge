9748241f3e15946da72ceaffe7cbf812af3e2517
NinV <quannl71290@gmail.com>
2020-09-11 00:14:10 +0900

    minor fix

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ starttime=`date +%s`
 echo "---------------------------------------------------"
 #get all videos names
 ROOT_PATH=$(pwd)
-python3 detect_and_track_final_submission.py --input /data/test_data
+python3 detect_and_track_final_submission.py --input /data/test_data --weights detection/yolov5/weights/yolov5x.pt
 cd counting
 python counting.py
 python submit.py
