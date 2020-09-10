8446dac8938831af6d336fdcc428aec84c2114ed
NinV <quannl71290@gmail.com>
2020-09-11 00:18:24 +0900

    minor fix

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ echo "---------------------------------------------------"
 ROOT_PATH=$(pwd)
 python3 detect_and_track_final_submission.py --input /data/test_data --weights detection/yolov5/weights/yolov5x.pt
 cd counting
-python counting.py
-python submit.py
+python3 counting.py
+python3 submit.py
 mv submit.txt /data/submission_output
