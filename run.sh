5e894fed3eeb81370308ff3a4a03743688d8cc36
NinV <quannl71290@gmail.com>
2020-09-16 14:16:42 +0900

    change default parameters for submission

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ echo "---------------------------------------------------"
 ROOT_PATH=$(pwd)
 
 # default batch_size = 12 for GTX2070 Super
-# python3 detect_and_track_final_submission.py --input /data/test_data  --output counting/track_results --weights detection/yolov5/weights/yolov5x.pt --batch_size 12
+# python3 detect_and_track_final_submission.py --input /data/test_data  --output counting/track_results --weights detection/yolov5/weights/yolov5x.pt --batch_size 12 --min_iou 0.1
 
 # for GTX 1080TI batch_size can be set to higher value
-python3 detect_and_track_final_submission.py --input /data/test_data --output counting/track_results --weights detection/yolov5/weights/yolov5x.pt --batch_size 20
+python3 detect_and_track_final_submission.py --input /data/test_data --output counting/track_results --weights detection/yolov5/weights/yolov5x.pt --batch_size 20 --min_iou 0.1
 
 cd counting
 python3 counting.py
